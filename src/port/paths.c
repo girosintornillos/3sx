df0cfa0d7427729c52da7490a617371c8cd9b1cd
GiRo SiNToRNiLLoS™ <31075572+girosintornillos@users.noreply.github.com>
2026-02-23 22:50:41 -0300

    Update paths.c

--- src/port/paths.c
+++ src/port/paths.c
@@ -2,16 +2,6 @@
 
 #include <SDL3/SDL.h>
 
-static const char* pref_path = NULL;
-
-const char* Paths_GetPrefPath() {
-    if (pref_path == NULL) {
-        pref_path = SDL_GetPrefPath("CrowdedStreet", "3SX");
-    }
-
-    return pref_path;
-}
-
 const char* Paths_GetBasePath() {
     return SDL_GetBasePath();
 }
