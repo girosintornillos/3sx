99be33ed20a54de256636a99c6342e9741c19501
GiRo SiNToRNiLLoS™ <31075572+girosintornillos@users.noreply.github.com>
2026-02-25 12:14:16 -0300

    Update resources.c

--- src/port/resources.c
+++ src/port/resources.c
@@ -3,6 +3,7 @@
 #include "port/sdl/sdl_app.h"
 
 #include <SDL3/SDL.h>
+#include <cdio/iso9660.h>
 
 typedef enum FlowState { INIT, DIALOG_OPENED, COPY_ERROR, COPY_SUCCESS } ResourceCopyingFlowState;
 
@@ -21,24 +22,68 @@ static bool check_if_file_present(const char* filename) {
     return result;
 }
 
-static bool copy_file(const char* rom_path, const char* src_name, const char* dst_name) {
-    char* src_path = NULL;
-    char* dst_path = Resources_GetPath(dst_name);
-    SDL_asprintf(&src_path, "%s/%s", rom_path, src_name);
-
-    const bool success = SDL_CopyFile(src_path, dst_path);
-
-    SDL_free(src_path);
-    SDL_free(dst_path);
-
-    return success;
+static void create_resources_directory() {
+    char* path = Resources_GetPath(NULL);
+    SDL_CreateDirectory(path);
+    SDL_free(path);
 }
 
-static void open_folder_dialog_callback(void* userdata, const char* const* filelist, int filter) {
-    const char* rom_path = filelist[0];
-    bool success = true;
-    success &= copy_file(rom_path, "SF33RD.AFS", "SF33RD.AFS");
-    flow_state = success ? COPY_SUCCESS : COPY_ERROR;
+#define CHUNK_SECTORS 16
+#define BUFFER_SIZE (ISO_BLOCKSIZE * CHUNK_SECTORS)
+
+static void open_file_dialog_callback(void* userdata, const char* const* filelist, int filter) {
+    const char* iso_path = filelist[0];
+
+    iso9660_t* iso = iso9660_open(iso_path);
+
+    if (iso == NULL) {
+        flow_state = COPY_ERROR;
+        return;
+    }
+
+    iso9660_stat_t* stat = iso9660_ifs_stat(iso, "/THIRD/SF33RD.AFS;1");
+
+    if (stat == NULL) {
+        // Try a different path
+        stat = iso9660_ifs_stat(iso, "/SF33RD.AFS;1");
+
+        if (stat == NULL) {
+            iso9660_close(iso);
+            flow_state = COPY_ERROR;
+            return;
+        }
+    }
+
+    create_resources_directory();
+    char* dst_path = Resources_GetPath("SF33RD.AFS");
+    SDL_IOStream* dst_io = SDL_IOFromFile(dst_path, "w");
+    SDL_free(dst_path);
+
+    uint8_t buffer[BUFFER_SIZE];
+    uint64_t bytes_remaining = stat->total_size;
+    lsn_t current_lsn = stat->lsn;
+
+    while (bytes_remaining > 0) {
+        const uint64_t bytes_to_read = SDL_min(sizeof(buffer), bytes_remaining);
+        const uint64_t sectors_to_read = (bytes_to_read + ISO_BLOCKSIZE - 1) / ISO_BLOCKSIZE;
+
+        const long bytes_read = iso9660_iso_seek_read(iso, buffer, current_lsn, sectors_to_read);
+        SDL_WriteIO(dst_io, buffer, bytes_read);
+
+        bytes_remaining -= bytes_read;
+        current_lsn += sectors_to_read;
+    }
+
+    iso9660_stat_free(stat);
+    iso9660_close(iso);
+    SDL_CloseIO(dst_io);
+    flow_state = COPY_SUCCESS;
+}
+
+static void open_dialog() {
+    flow_state = DIALOG_OPENED;
+    const SDL_DialogFileFilter filter = { .name = "Game iso", .pattern = "iso" };
+    SDL_ShowOpenFileDialog(open_file_dialog_callback, NULL, window, &filter, 1, NULL, false);
 }
 
 char* Resources_GetPath(const char* file_path) {
@@ -65,10 +110,9 @@ bool Resources_RunResourceCopyingFlow() {
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION,
                                  "Resources are missing",
                                  "3SX needs resources from a copy of \"Street Fighter III: 3rd Strike\" to run. Choose "
-                                 "a location with the game files in the next dialog",
+                                 "the iso in the next dialog",
                                  window);
-        flow_state = DIALOG_OPENED;
-        SDL_ShowOpenFolderDialog(open_folder_dialog_callback, NULL, window, NULL, false);
+        open_dialog();
         break;
 
     case DIALOG_OPENED:
@@ -76,12 +120,9 @@ bool Resources_RunResourceCopyingFlow() {
         break;
 
     case COPY_ERROR:
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
-                                 "Invalid directory",
-                                 "The directory you provided doesn't contain the required files",
-                                 window);
-        flow_state = DIALOG_OPENED;
-        SDL_ShowOpenFolderDialog(open_folder_dialog_callback, NULL, window, NULL, false);
+        SDL_ShowSimpleMessageBox(
+            SDL_MESSAGEBOX_ERROR, "Invalid iso", "The iso you provided doesn't contain the required files", window);
+        open_dialog();
         break;
 
     case COPY_SUCCESS:
